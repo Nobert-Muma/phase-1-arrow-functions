05095e3300007dfc18812244f73ffc96c2d967b9
Nobert-Muma <mumanobert17@gmail.com>
2024-06-25 18:26:41 +0300

    Working with arrow functions

---
+++ index.js
@@ -0,0 +1,10 @@
+// index.js
+
+function divide(a, b) {
+    return a / b;
+  }
+  const square = (x) => x * x;
+  const add = (x, y) => x + y;
+  const result = divide(2000, 100);
+  console.log(result); 
+  
